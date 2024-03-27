dabd41f2e235c139a02b7ff027d86de77f8db9d0
Novalunosis97 <157815071+Novalunosis97@users.noreply.github.com>
2024-03-27 01:54:44 +0000

    l

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+python
+Copy code
 import os
 import cv2
 import numpy as np
 import streamlit as st
 from tensorflow.keras.models import load_model
-from info_page import show_info_page 
 
 # Load the model
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -14,10 +15,68 @@ model = load_model(model_file_path)
 img_length = 50
 img_width = 50
 
+# Define CSS styles dynamically based on theme settings
+def generate_css(primary_color, secondary_background_color):
+    css = f"""
+    <style>
+        body {{
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #ffffff; /* Set background color to white */
+        }}
+        .container {{
+            display: flex;
+            flex-direction: column; /* Change flex-direction to column */
+            align-items: center; /* Align items to center */
+            height: 100vh;
+            justify-content: center; /* Vertically center content */
+        }}
+        .input-side, .output-side {{
+            width: 80%; /* Adjust width to take up 80% of the container */
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            margin-bottom: 20px; /* Add margin to separate input and output sides */
+        }}
+        .input-side {{
+            background-color: {secondary_background_color}; /* Use secondary background color */
+        }}
+        .output-side {{
+            background-color: #fff;
+        }}
+        .title {{
+            font-size: 2rem;
+            color: {primary_color}; /* Use primary color for title */
+            margin-bottom: 10px; /* Reduce margin bottom for title */
+        }}
+        .button {{
+            background-color: {primary_color}; /* Use primary color for buttons */
+            color: #ffffff; /* Set text color to white */
+            border: none;
+            border-radius: 5px;
+            padding: 10px 20px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+        }}
+        .button:hover {{
+            background-color: #4786a5; /* Darken the background color on hover */
+        }}
+        .grad-cam {{
+            max-width: 50px; /* Set maximum width for grad cam image */
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }}
+    </style>
+    """
+    return css
+
 def process_image(img):
     img = cv2.resize(img, (img_length, img_width))
     input_data = np.array([img], dtype=np.float32) / 255.0
-    return input_data
+    prediction = model.predict(input_data)
+    result = "True" if prediction[0][0] > 0.5 else "False"
+    return result, prediction[0][0]
 
 def process_video(video_path):
     video = cv2.VideoCapture(video_path)
@@ -31,6 +90,15 @@ def process_video(video_path):
     return frames
 
 def main():
+    # Get theme settings from config.toml
+    primary_color = st.config.get_option("theme.primaryColor")
+    secondary_background_color = st.config.get_option("theme.secondaryBackgroundColor")
+
+    # Render CSS styles
+    css = generate_css(primary_color, secondary_background_color)
+    st.markdown(css, unsafe_allow_html=True)
+
+    # Main content
     page = st.sidebar.selectbox("Go to", ["PolypDetect", "Info Page", "Comments", "QR Code"])
 
     if page == "PolypDetect":
@@ -38,37 +106,43 @@ def main():
         st.write("""
         This website utilizes a Machine Learning Model to detect polyps in the colon.
         Polyps are clumps of cells that form on the lining of the colon.
-        Polyps have been linked to high severity in patients who have an Inflammatory Bowel Disease (IBS).
+        Polyps have been linked to high severity in patients who have an Inflammatory Bowl Disease (IBS).
         This website can help doctors to ensure that they identify all polyps, as some can be discrete.
         Please remember that the model is not perfect, so use it as a second method.
         """)
 
-        uploaded_file = st.file_uploader("Upload Image or Video")
+        # Input side
+        st.markdown('<div class="input-side">', unsafe_allow_html=True)
+        st.markdown('<h2 class="title" style="color: #4786a5;">Upload Image or Video</h2>', unsafe_allow_html=True)  # Mellow blue color
+        uploaded_file = st.file_uploader("Choose an image or video...", type=["jpg", "jpeg", "png", "mp4"])
+        st.markdown('</div>', unsafe_allow_html=True)
 
-        if uploaded_file:
-            if uploaded_file.type.startswith('image'):
-                img = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-                img = cv2.imdecode(img, cv2.IMREAD_COLOR)
-                st.image(img, caption='Uploaded Image', use_column_width=True)
-
-            elif uploaded_file.type.startswith('video'):
-                video_path = os.path.join(script_dir, 'temp_video.mp4')  # Temporarily save video as .mp4
-                with open(video_path, 'wb') as f:
-                    f.write(uploaded_file.read())
-                frames = process_video(video_path)
-                selected_frame_index = st.select_slider('Select a frame for processing', range(len(frames)))
-                selected_frame = frames[selected_frame_index]
-                st.image(cv2.cvtColor(selected_frame, cv2.COLOR_BGR2RGB), caption='Selected Frame', channels='RGB', use_column_width=True)
-
-        if st.button('Detect Polyp'):
-            if uploaded_file is not None:
-                img = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-                img = cv2.imdecode(img, cv2.IMREAD_COLOR)
-                input_data = process_image(img)
-                prediction = model.predict(input_data)
-                result = "True" if prediction[0][0] > 0.5 else "False"
+        # Output side
+        st.markdown('<div class="output-side">', unsafe_allow_html=True)
+        if uploaded_file is not None:
+            st.markdown('<h2 class="title" style="color: #4786a5;">Detection Result</h2>', unsafe_allow_html=True)  # Mellow blue color
+            # Perform detection and display result
+            if st.button('Detect Polyps'):
+                st.write("Performing detection...")  # Placeholder for actual detection process
+                # Placeholder for displaying detected result
+                if uploaded_file.type.startswith('image'):
+                    img = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+                    img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+                    result, probability = process_image(img)
+                    # Display the original image
+                    st.image(img, caption='Original Image', use_column_width=True)
+                elif uploaded_file.type.startswith('video'):
+                    video_path = os.path.join(script_dir, 'temp_video.mp4')  # Temporarily save video as .mp4
+                    with open(video_path, 'wb') as f:
+                        f.write(uploaded_file.read())
+                    frames = process_video(video_path)
+                    selected_frame_index = st.select_slider('Select a frame for processing', range(len(frames)))
+                    selected_frame = frames[selected_frame_index]
+                    result, probability = process_image(selected_frame)
+                    # Display a preview image of the selected frame
+                    st.image(cv2.cvtColor(selected_frame, cv2.COLOR_BGR2RGB), caption='Selected Frame', channels='RGB', use_column_width=True)
                 st.write(f"Prediction: {result}")
-                st.write(f"Probability: {prediction[0][0]}")
+                st.write(f"Probability: {probability}")
 
     elif page == "Info Page":
         show_info_page(primary_color, secondary_background_color)  # Call the show_info_page function with theme colors
