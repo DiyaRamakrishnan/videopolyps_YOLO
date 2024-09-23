1dea6fa46cca2a7032dac5a42a673e704e045a7d
Novalunosis97 <157815071+Novalunosis97@users.noreply.github.com>
2024-09-23 00:46:05 +0000

    l

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ import numpy as np
 import streamlit as st
 from tensorflow.keras.models import load_model
 from info_page import show_info_page 
+import torch
 
-# Load the model
+# Load the models
 script_dir = os.path.dirname(os.path.abspath(__file__))
 model_file_path = os.path.join(script_dir, 'models', 'model_1.h5')
 model = load_model(model_file_path)
+
+# Load YOLOv5 model
+yolo_model = torch.hub.load('ultralytics/yolov5', 'custom', path=os.path.join(script_dir, 'models', 'best.pt'))
 
 # Define image dimensions
 img_length = 50
@@ -22,36 +26,36 @@ def generate_css(primary_color, secondary_background_color):
             font-family: 'Arial', sans-serif;
             margin: 0;
             padding: 0;
-            background-color: #ffffff; /* Set background color to white */
+            background-color: #ffffff;
         }}
         .container {{
             display: flex;
-            flex-direction: column; /* Change flex-direction to column */
-            align-items: center; /* Align items to center */
+            flex-direction: column;
+            align-items: center;
             height: 100vh;
-            justify-content: center; /* Vertically center content */
+            justify-content: center;
         }}
         .input-side, .output-side {{
-            width: 80%; /* Adjust width to take up 80% of the container */
+            width: 80%;
             padding: 20px;
             border-radius: 10px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            margin-bottom: 20px; /* Add margin to separate input and output sides */
+            margin-bottom: 20px;
         }}
         .input-side {{
-            background-color: {secondary_background_color}; /* Use secondary background color */
+            background-color: {secondary_background_color};
         }}
         .output-side {{
             background-color: #fff;
         }}
         .title {{
             font-size: 2rem;
-            color: {primary_color}; /* Use primary color for title */
-            margin-bottom: 10px; /* Reduce margin bottom for title */
+            color: {primary_color};
+            margin-bottom: 10px;
         }}
         .button {{
-            background-color: {primary_color}; /* Use primary color for buttons */
-            color: #ffffff; /* Set text color to white */
+            background-color: {primary_color};
+            color: #ffffff;
             border: none;
             border-radius: 5px;
             padding: 10px 20px;
@@ -59,7 +63,7 @@ def generate_css(primary_color, secondary_background_color):
             transition: background-color 0.3s;
         }}
         .button:hover {{
-            background-color: #4786a5; /* Darken the background color on hover */
+            background-color: #4786a5;
         }}
         .prediction {{
             font-size: 1.5rem;
@@ -70,7 +74,7 @@ def generate_css(primary_color, secondary_background_color):
             margin-bottom: 20px;
         }}
         .output-image {{
-            max-width: 400px; /* Set maximum width for output image */
+            max-width: 400px;
             border-radius: 8px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }}
@@ -79,11 +83,26 @@ def generate_css(primary_color, secondary_background_color):
     return css
 
 def process_image(img):
-    img = cv2.resize(img, (img_length, img_width))
-    input_data = np.array([img], dtype=np.float32) / 255.0
+    # Resize for the classification model
+    resized_img = cv2.resize(img, (img_length, img_width))
+    input_data = np.array([resized_img], dtype=np.float32) / 255.0
     prediction = model.predict(input_data)
     result = "True" if prediction[0][0] > 0.5 else "False"
-    return result, prediction[0][0]
+    
+    # If classified as having a polyp, perform YOLO detection
+    if result == "True":
+        # Convert BGR to RGB
+        rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        
+        # Perform YOLO detection
+        results = yolo_model(rgb_img)
+        
+        # Draw bounding boxes
+        img_with_boxes = results.render()[0]
+        
+        return result, prediction[0][0], img_with_boxes
+    
+    return result, prediction[0][0], img
 
 def process_video(video_path, frame_number):
     video = cv2.VideoCapture(video_path)
@@ -116,39 +135,37 @@ def main():
 
         # Input side
         st.markdown('<div class="input-side">', unsafe_allow_html=True)
-        st.markdown('<h2 class="title" style="color: #4786a5;">Upload Image or Video</h2>', unsafe_allow_html=True)  # Mellow blue color
+        st.markdown('<h2 class="title" style="color: #4786a5;">Upload Image or Video</h2>', unsafe_allow_html=True)
         uploaded_file = st.file_uploader("Choose an image or video...", type=["jpg", "jpeg", "png", "mp4", "mov"])
         st.markdown('</div>', unsafe_allow_html=True)
 
         # Output side
         st.markdown('<div class="output-side">', unsafe_allow_html=True)
         if uploaded_file is not None:
-            st.markdown('<h2 class="title" style="color: #4786a5;">Detection Result</h2>', unsafe_allow_html=True)  # Mellow blue color
-            # Perform detection and display result
+            st.markdown('<h2 class="title" style="color: #4786a5;">Detection Result</h2>', unsafe_allow_html=True)
             if uploaded_file.type.startswith('image'):
                 img = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
                 img = cv2.imdecode(img, cv2.IMREAD_COLOR)
-                if st.button('Detect Polyps'):  # Move the button outside of the condition
-                    result, probability = process_image(img)
-                    # Display the original image
+                if st.button('Detect Polyps'):
+                    result, probability, processed_img = process_image(img)
                     st.markdown(f'<p class="prediction">Prediction: {result}</p>', unsafe_allow_html=True)
                     st.markdown(f'<p class="probability">Model Output: {probability}</p>', unsafe_allow_html=True)
-                    st.image(img, caption='Original Image', width=500, output_format='JPEG')
+                    st.image(processed_img, caption='Processed Image', width=500, channels="RGB")
             elif uploaded_file.type.startswith('video'):
-                video_path = os.path.join(script_dir, 'temp_video.mp4')  # Temporarily save video as .mp4
+                video_path = os.path.join(script_dir, 'temp_video.mp4')
                 with open(video_path, 'wb') as f:
                     f.write(uploaded_file.read())
                 frame_number = st.number_input("Frame Number", value=0, step=1)
                 selected_frame = process_video(video_path, frame_number)
-                st.image(cv2.cvtColor(selected_frame, cv2.COLOR_BGR2RGB), caption='Selected Frame', channels='RGB', width=500, output_format='JPEG')
-                st.markdown('<h2 class="title" style="color: #4786a5;">Detection Result</h2>', unsafe_allow_html=True)  # Mellow blue color
-                # Perform detection and display result
-                result, probability = process_image(selected_frame)
-                st.markdown(f'<p class="prediction">Prediction: {result}</p>', unsafe_allow_html=True)
-                st.markdown(f'<p class="probability">Probability: {probability}</p>', unsafe_allow_html=True)
+                st.image(cv2.cvtColor(selected_frame, cv2.COLOR_BGR2RGB), caption='Selected Frame', channels='RGB', width=500)
+                if st.button('Detect Polyps'):
+                    result, probability, processed_frame = process_image(selected_frame)
+                    st.markdown(f'<p class="prediction">Prediction: {result}</p>', unsafe_allow_html=True)
+                    st.markdown(f'<p class="probability">Model Output: {probability}</p>', unsafe_allow_html=True)
+                    st.image(processed_frame, caption='Processed Frame', width=500, channels="RGB")
                 
     elif page == "Info Page":
-        show_info_page(primary_color, secondary_background_color)  # Call the show_info_page function with theme colors
+        show_info_page(primary_color, secondary_background_color)
 
     elif page == "QR Code":
         st.title("QR Code")
