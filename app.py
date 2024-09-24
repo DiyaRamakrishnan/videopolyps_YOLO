ab997819f5e9d1a242e22d1351b1e39dc98d7b42
Novalunosis97 <157815071+Novalunosis97@users.noreply.github.com>
2024-09-24 00:56:41 +0000

    l

--- app.py
+++ app.py
@@ -4,11 +4,16 @@ import numpy as np
 import streamlit as st
 from tensorflow.keras.models import load_model
 from info_page import show_info_page 
+import torch
 
-# Load the model
+# Load the CNN model
 script_dir = os.path.dirname(os.path.abspath(__file__))
-model_file_path = os.path.join(script_dir, 'models', 'model_1.h5')
-model = load_model(model_file_path)
+cnn_model_file_path = os.path.join(script_dir, 'models', 'model_1.h5')
+cnn_model = load_model(cnn_model_file_path)
+
+# Load the YOLO model
+yolo_model_file_path = os.path.join(script_dir, 'models', 'best.pt')
+yolo_model = torch.hub.load('ultralytics/yolov5', 'custom', path=yolo_model_file_path)
 
 # Define image dimensions
 img_length = 50
@@ -22,36 +27,36 @@ def generate_css(primary_color, secondary_background_color):
             font-family: 'Arial', sans-serif;
             margin: 0;
             padding: 0;
-            background-color: #ffffff; /* Set background color to white */
+            background-color: #ffffff;
         }}
         .container {{
             display: flex;
-            flex-direction: column; /* Change flex-direction to column */
-            align-items: center; /* Align items to center */
+            flex-direction: column;
+            align-items: center;
             height: 100vh;
-            justify-content: center; /* Vertically center content */
+            justify-content: center;
         }}
         .input-side, .output-side {{
-            width: 80%; /* Adjust width to take up 80% of the container */
+            width: 80%;
             padding: 20px;
             border-radius: 10px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            margin-bottom: 20px; /* Add margin to separate input and output sides */
+            margin-bottom: 20px;
         }}
         .input-side {{
-            background-color: {secondary_background_color}; /* Use secondary background color */
+            background-color: {secondary_background_color};
         }}
         .output-side {{
             background-color: #fff;
         }}
         .title {{
             font-size: 2rem;
-            color: {primary_color}; /* Use primary color for title */
-            margin-bottom: 10px; /* Reduce margin bottom for title */
+            color: {primary_color};
+            margin-bottom: 10px;
         }}
         .button {{
-            background-color: {primary_color}; /* Use primary color for buttons */
-            color: #ffffff; /* Set text color to white */
+            background-color: {primary_color};
+            color: #ffffff;
             border: none;
             border-radius: 5px;
             padding: 10px 20px;
@@ -59,7 +64,7 @@ def generate_css(primary_color, secondary_background_color):
             transition: background-color 0.3s;
         }}
         .button:hover {{
-            background-color: #4786a5; /* Darken the background color on hover */
+            background-color: #4786a5;
         }}
         .prediction {{
             font-size: 1.5rem;
@@ -70,7 +75,7 @@ def generate_css(primary_color, secondary_background_color):
             margin-bottom: 20px;
         }}
         .output-image {{
-            max-width: 400px; /* Set maximum width for output image */
+            max-width: 400px;
             border-radius: 8px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }}
@@ -78,12 +83,16 @@ def generate_css(primary_color, secondary_background_color):
     """
     return css
 
-def process_image(img):
+def process_image_cnn(img):
     img = cv2.resize(img, (img_length, img_width))
     input_data = np.array([img], dtype=np.float32) / 255.0
-    prediction = model.predict(input_data)
+    prediction = cnn_model.predict(input_data)
     result = "True" if prediction[0][0] > 0.5 else "False"
     return result, prediction[0][0]
+
+def process_image_yolo(img):
+    results = yolo_model(img)
+    return results
 
 def process_video(video_path, frame_number):
     video = cv2.VideoCapture(video_path)
@@ -107,48 +116,91 @@ def main():
     if page == "PolypDetect":
         st.title('PolypDetect')
         st.write("""
-        This website utilizes a Machine Learning Model to detect polyps in the colon.
+        This website utilizes Machine Learning Models to detect polyps in the colon.
         Polyps are clumps of cells that form on the lining of the colon.
-        Polyps have been linked to high severity in patients who have an Inflammatory Bowl Disease (IBS).
+        Polyps have been linked to high severity in patients who have an Inflammatory Bowel Disease (IBD).
         This website can help doctors to ensure that they identify all polyps, as some can be discrete.
         Please remember that the model is not perfect, so use it as a second method.
         """)
 
         # Input side
         st.markdown('<div class="input-side">', unsafe_allow_html=True)
-        st.markdown('<h2 class="title" style="color: #4786a5;">Upload Image or Video</h2>', unsafe_allow_html=True)  # Mellow blue color
+        st.markdown('<h2 class="title" style="color: #4786a5;">Upload Image or Video</h2>', unsafe_allow_html=True)
         uploaded_file = st.file_uploader("Choose an image or video...", type=["jpg", "jpeg", "png", "mp4", "mov"])
         st.markdown('</div>', unsafe_allow_html=True)
 
         # Output side
         st.markdown('<div class="output-side">', unsafe_allow_html=True)
         if uploaded_file is not None:
-            st.markdown('<h2 class="title" style="color: #4786a5;">Detection Result</h2>', unsafe_allow_html=True)  # Mellow blue color
-            # Perform detection and display result
+            st.markdown('<h2 class="title" style="color: #4786a5;">Detection Result</h2>', unsafe_allow_html=True)
+            
             if uploaded_file.type.startswith('image'):
                 img = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
                 img = cv2.imdecode(img, cv2.IMREAD_COLOR)
-                if st.button('Detect Polyps'):  # Move the button outside of the condition
-                    result, probability = process_image(img)
+                if st.button('Detect Polyps'):
+                    # CNN prediction
+                    cnn_result, cnn_probability = process_image_cnn(img)
+                    st.markdown(f'<p class="prediction">CNN Prediction: {cnn_result}</p>', unsafe_allow_html=True)
+                    st.markdown(f'<p class="probability">CNN Model Output: {cnn_probability:.4f}</p>', unsafe_allow_html=True)
+                    
+                    # If CNN predicts a polyp, perform YOLO detection
+                    if cnn_result == "True":
+                        yolo_results = process_image_yolo(img)
+                        
+                        # Display YOLO results
+                        st.image(yolo_results.render()[0], caption='YOLO Detection', use_column_width=True)
+                        
+                        # Display detection information
+                        for detection in yolo_results.xyxy[0]:
+                            confidence = detection[4].item()
+                            class_id = int(detection[5].item())
+                            class_name = yolo_model.names[class_id]
+                            st.write(f"Detected: {class_name}, Confidence: {confidence:.4f}")
+                    else:
+                        st.write("No polyp detected by CNN model.")
+                    
                     # Display the original image
-                    st.markdown(f'<p class="prediction">Prediction: {result}</p>', unsafe_allow_html=True)
-                    st.markdown(f'<p class="probability">Model Output: {probability}</p>', unsafe_allow_html=True)
-                    st.image(img, caption='Original Image', width=500, output_format='JPEG')
+                    st.image(img, caption='Original Image', use_column_width=True)
+            
             elif uploaded_file.type.startswith('video'):
-                video_path = os.path.join(script_dir, 'temp_video.mp4')  # Temporarily save video as .mp4
+                video_path = os.path.join(script_dir, 'temp_video.mp4')
                 with open(video_path, 'wb') as f:
                     f.write(uploaded_file.read())
-                frame_number = st.number_input("Frame Number", value=0, step=1)
-                selected_frame = process_video(video_path, frame_number)
-                st.image(cv2.cvtColor(selected_frame, cv2.COLOR_BGR2RGB), caption='Selected Frame', channels='RGB', width=500, output_format='JPEG')
-                st.markdown('<h2 class="title" style="color: #4786a5;">Detection Result</h2>', unsafe_allow_html=True)  # Mellow blue color
-                # Perform detection and display result
-                result, probability = process_image(selected_frame)
-                st.markdown(f'<p class="prediction">Prediction: {result}</p>', unsafe_allow_html=True)
-                st.markdown(f'<p class="probability">Probability: {probability}</p>', unsafe_allow_html=True)
                 
+                video = cv2.VideoCapture(video_path)
+                total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+                video.release()
+
+                frame_number = st.slider("Select frame", 0, total_frames - 1, 0)
+                selected_frame = process_video(video_path, frame_number)
+                
+                if st.button('Detect Polyps'):
+                    # CNN prediction
+                    cnn_result, cnn_probability = process_image_cnn(selected_frame)
+                    st.markdown(f'<p class="prediction">CNN Prediction: {cnn_result}</p>', unsafe_allow_html=True)
+                    st.markdown(f'<p class="probability">CNN Model Output: {cnn_probability:.4f}</p>', unsafe_allow_html=True)
+                    
+                    # If CNN predicts a polyp, perform YOLO detection
+                    if cnn_result == "True":
+                        yolo_results = process_image_yolo(selected_frame)
+                        
+                        # Display YOLO results
+                        st.image(yolo_results.render()[0], caption='YOLO Detection', use_column_width=True)
+                        
+                        # Display detection information
+                        for detection in yolo_results.xyxy[0]:
+                            confidence = detection[4].item()
+                            class_id = int(detection[5].item())
+                            class_name = yolo_model.names[class_id]
+                            st.write(f"Detected: {class_name}, Confidence: {confidence:.4f}")
+                    else:
+                        st.write("No polyp detected by CNN model.")
+                    
+                    # Display the original frame
+                    st.image(cv2.cvtColor(selected_frame, cv2.COLOR_BGR2RGB), caption='Selected Frame', use_column_width=True)
+
     elif page == "Info Page":
-        show_info_page(primary_color, secondary_background_color)  # Call the show_info_page function with theme colors
+        show_info_page(primary_color, secondary_background_color)
 
     elif page == "QR Code":
         st.title("QR Code")
